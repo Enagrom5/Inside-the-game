ba0efe8818601a400b6b4af6c36f429239e62a33
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-12-11 20:51:51 +0100

    update SQL

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE `User` (
     `name` varchar(50)  NOT NULL ,
     `email` varchar(100)  NOT NULL ,
     `password` varchar(255)  NOT NULL ,
-    `image` varchar(255)  NOT NULL ,
+    `image` varchar(255),
     PRIMARY KEY (
         `id`
     )
